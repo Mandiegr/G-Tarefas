d656a0d45558fa086b9ca412cbcf1d66c1a1d1ae
Mandiegr <nannda076@gmail.com>
2024-08-28 22:41:11 -0300

    feat: add SearchForm functionality to Navbar for authenticated users

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import styled from 'styled-components';
@@ -7,19 +7,39 @@ import Menu from './Button';
 const Navbar = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleLogout = () => {
     logout();
     navigate('/');
   };
 
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?query=${searchQuery}`);
+  };
 
   return (
     <Nav>
       <LeftNavLinks>
-        <NavLink to="/">Home</NavLink>
         {isAuthenticated && <NavLink to="/tasks">Minhas Tarefas</NavLink>}
       </LeftNavLinks>
+
+      {isAuthenticated && (
+        <SearchForm onSubmit={handleSearchSubmit}>
+          <SearchInput
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            placeholder="Pesquisar..."
+          />
+          <SearchButton type="submit">Buscar</SearchButton>
+        </SearchForm>
+      )}
 
       <RightNavLinks>
         {isAuthenticated ? (
@@ -46,7 +66,7 @@ const Nav = styled.nav`
   padding: 1rem 2rem;
   background-color: #282c34;
 
-  @media (max-width: 768px) {
+  @media (max-width: 789px) {
     justify-content: center;
   }
 `;
@@ -55,7 +75,7 @@ const LeftNavLinks = styled.div`
   display: flex;
   align-items: center;
 
-  @media (max-width: 768px) {
+  @media (max-width: 789px) {
     display: none;
   }
 `;
@@ -64,7 +84,7 @@ const RightNavLinks = styled.div`
   display: flex;
   align-items: center;
 
-  @media (max-width: 768px) {
+  @media (max-width: 820px) {
     display: none;
   }
 `;
@@ -77,6 +97,39 @@ const NavLink = styled(Link)`
 
   &:hover {
     text-decoration: underline;
+  }
+`;
+
+const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  //justify-content: center;
+`;
+
+const SearchInput = styled.input`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-right: 0.5rem;
+  width: 400px;
+
+  @media (max-width: 768px) {
+    width: 60%;
+  }
+`;
+
+const SearchButton = styled.button`
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: wheat;
+  color: #282c34;
+  cursor: pointer;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #faedcd;
   }
 `;
 
